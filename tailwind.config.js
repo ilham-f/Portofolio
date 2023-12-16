3cbfa9eb6ff74f36809515e4ae266a18d8e60c93
Moh. Ilham Fahrezy <mifahrezy11@gmail.com>
2023-12-16 15:24:30 +0700

    responsive 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,12 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  breakpoints: {
+    'xs': '320px',
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+  }
 }
 
